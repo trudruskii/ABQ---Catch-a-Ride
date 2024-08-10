333a106747e311d2be403612118d3a032f4ee4b0
trudrewski <aspencer22@cnm.edu>
2024-08-09 20:48:40 -0600

    added essential static features for application to main branch -- switching to develop -- then to frontend

---
+++ frontend/src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return(
+        <>
+            <div className="bg-gray-800 w-full text-white text-center p-4 fixed bottom-0">
+                &copy; 2024 ABQ Catch a Ride
+            </div>
+        </>
+    )
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <Navbar />
       <body className={inter.className}>{children}</body>
+    <Footer />
     </html>
   );
 }
