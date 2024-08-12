cbc052686fb9becbd81c24818045dd1cadeca644
trudrewski <aspencer22@cnm.edu>
2024-08-11 21:51:35 -0600

    added content to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,8 +9,8 @@ export default function HomepageNotSignedIn() {
               <title>ABQ CAR</title>
               <link rel="logo" href="/favicon.ico" />
           </head>
-          <div className="grid grid-cols-2">
-
+          <div className="grid grid-cols-2 p-6">
+              <span>Hello neighbors! I hope that you aren't finding yourself in a situation to where you are stranded and desperately could use some help. If you did, that's okay! We got you covered! Take a look around and get connected with someone else local in Albuquerque to give you a hand by clicking on the Services tab and selecting which service fits your needs the best.</span>
 
           </div>
       </>
